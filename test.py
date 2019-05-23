7ac00be50d0d4050e4ce80a97be3628e15bf6a5f
fattynerd <40905726+fattynerd@users.noreply.github.com>
2019-05-23 23:05:21 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((1000,800))
-while True:
-    for event in pygame.event.get():
-       if event.type == pygame.KEYDOWN:
-            print(event.key)
