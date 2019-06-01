9e4e3bca45823cf887b8cf49d784b7983c06b48c
fattynerd <andrew.mutien@gmail.com>
2019-06-01 23:44:39 +0800

    finished presentation

---
+++ thankyou.py
@@ -0,0 +1,27 @@
+
+
+class Ending():
+    def __init__(self):
+        print('Thank everyone for this process')
+        print('Especially for Yushiuan')
+        print('He really Helped me alot')
+        print('also, thank all the people who drew for this project')
+        print('no matter if they were used or not')
+        print('Now this is it, thanks guys')
+    def abigone(self):
+        print('__________________   |                    |  ________________________  ||                      | |              /    ___________________  ')
+        print('        |            |                    |  |                      |  |  |                    | |            /      |                    ')
+        print('        |            |                    |  |                      |  |    |                  | |          /        |                    ')
+        print('        |            |                    |  |                      |  |      |                | |        /          |                    ')
+        print('        |            |                    |  |                      |  |        |              | |      /            |                    ')
+        print('        |            |                    |  |                      |  |          |            | |    /              |                    ')
+        print('        |            |____________________|  |______________________|  |            |          | |   /               |___________________ ')
+        print('        |            |                    |  |                      |  |              |        | |   |                                   |')
+        print('        |            |                    |  |                      |  |                |      | |     |                                 |')
+        print('        |            |                    |  |                      |  |                  |    | |       |                               |')
+        print('        |            |                    |  |                      |  |                    |  | |          |                            |')
+        print('        |            |                    |  |                      |  |                     | | |              |                        |')
+        print('        |            |                    |  |                      |  |                      || |                 |  ___________________|')
+
+ending = Ending()
+ending.abigone()
